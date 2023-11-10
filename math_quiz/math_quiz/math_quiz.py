91041717d184276168855e253d7c00f7260fdf4b
fapapate <patel@faps.com>
2023-11-10 11:34:07 +0100

    Cleaned Code with improved functionality

--- math_quiz/math_quiz/math_quiz.py
+++ math_quiz/math_quiz/math_quiz.py
@@ -1,46 +1,72 @@
 import random
 
 
-def function_A(min, max):
+def generate_random_integer(minimum_range, maximum_range):
     """
-    Random integer.
+    Returns a random integer specified within the minimum and maximum range
     """
-    return random.randint(min, max)
+    return random.randint(minimum_range, maximum_range)
 
 
-def function_B():
+def generate_random_operator():
+    """
+    Generates a random mathematical operator where '+' is addition; '-' is subtraction; '*' is multiplication
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def calculation(number_1, number_2, operator):
+    """
+
+    :param number_1: Any integer number 1
+    :param number_2: Any integer number 2
+    :param operator: '+'/ '-'/ '*'
+    :return: Result
+    """
+    operation = f"{number_1} {operator} {number_2}"
+    if operator == '+':
+        result = number_1+number_2
+    elif operator == '-':
+        result = number_1-number_2
+    else:
+        result = number_1*number_2
+
+    return operation, result
+
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    """
+    Try-Except warning returns an invalid input format if user does not mention integer
+    :return: Final score based upon right answers
+    """
+    score = 0
+    total_questions = 4
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for question_number in range(total_questions):
+        number1 = generate_random_integer(1, 10)
+        number2 = generate_random_integer(1, 6)
+        operator = generate_random_operator()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        operation, result = calculation(number1, number2, operator)
+        print(f"\nQuestion: {operation}")
+        user_answer = input("Your answer: ")
 
-        if useranswer == ANSWER:
+        try:
+            user_answer = int(user_answer)
+        except ValueError:
+            print("Invalid input format. The function demands an integer input")
+
+        if user_answer == result:
             print("Correct! You earned a point.")
-            s += -(-1)
+            score += 1
         else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+            print(f"Wrong answer. The correct answer is {result}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {score}/{total_questions}")
+
 
 if __name__ == "__main__":
     math_quiz()
